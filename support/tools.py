ab505b1cf23e5bf80cef6b74da7296da61b152a1
Aditya Raj <adityarajsinghchouhan11@gmail.com>
2026-06-30 16:31:10 +0530

    created support app and tools

---
+++ support/tools.py
@@ -0,0 +1,52 @@
+from orders.models import Order,RefundRequest
+from django.utils import timezone
+from .tracking_data import DELIVERY_DATA
+
+def get_order_details(order_id):
+    try:
+        order=Order.objects.get(id=order_id)
+        return{
+            "order_id":order.id,
+            "product_name":order.product_name,
+            "amount":order.amount,
+            "status":order.status,
+            "carrier":order.carrier,
+            "tracking_number":order.tracking_number,
+            "delivery_address":order.delivery,
+            "ordered_on":order.created_at.strftime("%d %b %Y"),# 12 jan 2026
+            "days_since_order":(timezone()-order.created_at).days #5
+        }
+    except Order.DoesNotExist:
+        return {'error',f"Order #{order.id} not found."}
+    
+
+def get_refund_history(user_id):
+    refunds=RefundRequest.objects.filter(id=user_id).order_by('-created_at')
+
+    history=[]
+    for refund in refunds:
+        history.append({
+            "order_id":refund.order.id,
+            "product":refund.order.product,
+            "reason":refund.reason,
+            "status":refund.status,
+            "request_on":refund.created_at.strftime("%d %b %Y"),# 14 jan 2026
+        })
+    return {
+        "total_refund_requests":len(history),
+        "history":history,
+    }
+
+def check_delivery_status(tracking_number,carrier):
+    default_response={
+        "status": "Unknown",
+        "last_location": "Tracking info unavailable",
+        "last_update": "N/A",
+        "estimated_delivery": "Contact to carrier directly",
+        "delay_reason": "No update from the carrier",
+    }
+    result=DELIVERY_DATA.get(tracking_number,default_response)
+    result["tracking_number"]=tracking_number
+    result["carrier"]=carrier
+    return result
+
